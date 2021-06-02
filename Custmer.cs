4910b8622ce1cd55f488361d37de8c59a6dc36cf
baibhav-pixel <85218875+baibhav-pixel@users.noreply.github.com>
2021-06-02 14:59:13 +0530

    Add files via upload

---
+++ Custmer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ConsoleApp1
+{
+    class Custmer
+    {
+        string name;
+        int savings;
+
+        public void acceptdet(string n ,int s)
+        {
+            Console.WriteLine("Enter name and savings");
+            name = n;
+            savings = s;
+            
+        }
+
+        public void TraceInfor(Exception e)
+        {
+            Trace.Listeners.Add(new TextWriterTraceListener("traced.log"));
+            Trace.WriteLine(e.Message + " " + e.Source + " " + e.StackTrace);
+            Trace.WriteLine("traced ERRORS at " + DateTime.Now);
+            Trace.WriteLine("Traicing done by Decoders");
+            Trace.Flush();
+            Trace.Close();
+        }
+
+        public void getdetails()
+        {
+            Console.WriteLine("The name and address is ");
+            Console.WriteLine(name);
+            Console.WriteLine(savings);
+        }
+    }
+}
