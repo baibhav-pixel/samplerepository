284a677bc2541010e455efa9f3d92885dfde6689
baibhav-pixel <85218875+baibhav-pixel@users.noreply.github.com>
2021-06-02 14:31:55 +0530

    Add files via upload

---
+++ D8T5.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class D8T5
+    {
+        static void Main(string[] args)
+        {
+            Custmer obj = new Custmer();
+            Console.WriteLine("Enter name, savings");
+            try
+            {
+                string n = Console.ReadLine();
+                int s = Convert.ToInt32(Console.ReadLine());
+                obj.acceptdet(n, s);
+                obj.getdetails();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Some error occurred");
+                obj.TraceInfor(e);
+            }
+            Console.ReadKey();
+        }
+    }
+}
